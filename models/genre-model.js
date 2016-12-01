a478975d2ad8abd59f7bb70c9539642bb7e7e36e
electrone901 <Carbajalluis@rocketmail.com>
2016-12-01 14:52:23 -0500

    add genre-models

--- models/genre-model.js
+++ models/genre-model.js
@@ -1,9 +1,13 @@
 const Sequelize = require('sequelize');
 const sequelizeConnection = require('../db');
+const Song = require('./genre-models');
 
-//////////
-// YOUR CODE HERE:
-//////////
+var Genre = sequelizeConnection.define('genre', {
+  title: {
+    type: Sequelize.STRING
+    validate: {is: /^[a-zA -Z0-9]{1,250}$/} // validate input
+  }
+});
 
-
+Genre.belongsToMany(Song,{through 'song-genre'})
 module.exports = Genre;
