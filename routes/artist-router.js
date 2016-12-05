0d01712243559fea118da61f502b379f7e66a9b7
electrone901 <Carbajalluis@rocketmail.com>
2016-12-05 11:28:20 -0500

    add song and artist routes

--- routes/artist-router.js
+++ routes/artist-router.js
@@ -11,8 +11,49 @@ const getAllArtists = (req,res)=>(
   )
 
 
+const getOneArtist = (req,res)=> {
+	Artist.findById(req.params.id)
+	// Artist.findById(req.params.id)
+	.then((data)=>
+   	res.send(data)
+   	)
+}
+
+
+const createArtist = (req,res)=>{
+	Artist.create({name: req.body.name})
+	.then((data)=>{res.send(data)})
+}
+
+
+// /api/artists/:id 
+const deleteArtist = (req,res)=>{
+	Artist.destroy({where:{id:req.params.id}})
+	.then((id)=>{res.send(id.name + ' has been deleted!')})
+}
+
+
+const updateArtist = (req,res)=>{
+	Artist.update(
+		{name:req.params.newName},
+		{where: {id:req.params.id}}
+	)
+	.then((id)=>{Artist.findById(parseInt(id))})
+	.then((data)=>{res.send(data + ' updated!')})
+}
+
+
+//
 //ROUTES//
 router.route('/')
  .get(getAllArtists)
+ .post(createArtist)
+
+router.route('/:id')
+ .get(getOneArtist)
+ .delete(deleteArtist)
+
+router.route('/:id/:newName')
+  .put(updateArtist)
 
 module.exports = router
