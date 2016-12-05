fb46d5ef3bcb717f7285f8efe7b6c6e04783bc6a
Jung Chen <jungwchen@gmail.com>
2016-12-05 11:35:29 -0500

    add genre and playlist router

--- routes/genre-router.js
+++ routes/genre-router.js
@@ -3,8 +3,67 @@ const express = require('express');
 const router = express.Router();
 
 //FUNCTIONS//
+const getAllGenres = (req,res)=>(
+	Genre.findAll({
+    order:[['title', 'ASC']] 
+  })
+	.then(genreInfo=>
+    res.send(genreInfo)
+  )
+)
+
+const getGenresById = (req,res)=>(
+  Genre.findOne({
+    where: {id: req.params.id} 
+  })
+  .then(GenreId=>
+    res.send(GenreId)
+  )
+)
+
+const postNewGenre = (req,res)=>{
+	let body = req.body;
+  Genre.create({
+  	title: body.title
+  })
+  .then(()=>
+    res.send(body.title+' genre created!')
+  )
+}
+
+const deleteGenreById = (req,res)=>(
+  Genre.destroy({ 
+    where: { id: req.params.id } 
+  })
+  .then(()=> 
+    res.send('Genre with id: '+req.params.id+' has been deleted')
+  )
+)
+	
+const updateGenre = (req,res)=>(
+  Genre.findOne({ 
+    where: {id: req.params.id} 
+  })
+  .then(genreInfo=>
+		genreInfo.update({
+  	  title: req.params.newGenre 
+    })
+  )
+  .then(()=> 
+    res.send('Genre with Id:'+req.params.id+' has been updated to '+req.params.newGenre)
+  )
+)
 
 //ROUTES//
+router.route('/')
+  .get(getAllGenres)
+  .post(postNewGenre)
 
+router.route('/:id/:newGenre')
+  .put(updateGenre)
+
+router.route('/:id')
+  .get(getGenresById)
+  .delete(deleteGenreById)
 
 module.exports = router
--- routes/playlist-router.js
+++ routes/playlist-router.js
@@ -1,10 +1,116 @@
 const Playlist = require('../models/playlist-model');
+const Song = require('../models/song-model');
 const express = require('express');
 const router = express.Router();
 
 //FUNCTIONS//
+const getAllPlaylists = (req,res) =>(
+  Playlist.findAll({ 
+  	include: [Song] 
+  })
+  .then(playlistInfo=>
+  	res.send(playlistInfo)
+  )
+)
+
+const getPlaylistById = (req,res)=>(
+	Playlist.findOne({ 
+		where: {id: req.params.id}, 
+		include: [Song] 
+	})
+	.then(playlistId=>
+		res.send(playlistId)
+	)
+)
+
+const postNewPlaylist = (req,res)=>{
+	let body = req.body;
+	Playlist.create({
+		title: body.title
+	})
+	.then(()=>
+		res.send('Playlist with name '+body.title+' created!')
+	)
+}
+
+const updatePlaylistSong = (req,res)=>{
+	Song.findOne({ 
+		where: { title: req.params.songName } 
+	})
+	.then((songInfo)=>{
+		Playlist.findOne({ 
+			where: {id: req.params.id}
+		})
+		.then(playlistInfo=>{
+			playlistInfo.addSongs([songInfo.dataValues.id])
+		})
+	})
+	.then(()=>
+		res.send('Playlist ID:'+req.params.id+' updated with song: '+req.params.songName)
+	)
+}
+
+const removePlaylistSong = (req,res)=>{
+	Song.findOne({
+		where: {title: req.params.songName}
+	})
+	.then((songInfo)=>
+		Playlist.findOne({
+			where: {id: req.params.id}
+		})
+		.then((playlistInfo)=>(
+			playlistInfo.getSongs()
+		)
+		.then((playlistSongArray)=>{
+      let playlistSongIdArray = playlistSongArray.map((songIDs)=>songIDs.dataValues.id);
+      let songIndex = songInfo.dataValues.id;
+			let index = playlistSongIdArray.indexOf(songIndex);
+			if(index > -1){playlistSongIdArray.splice(index, 1)};
+			let newSongArray = playlistSongIdArray;
+			playlistInfo.setSongs(newSongArray)
+    })
+	)
+	.then(()=>
+		res.send('Removed '+req.params.songName+' from playlist with Id:'+req.params.id)
+	)
+	)
+}
+
+const deletePlaylistById = (req,res)=>(
+	Playlist.destroy({ 
+		where: { id: req.params.id } 
+	})
+  .then(()=> 
+  	res.send('Playlist with id: '+req.params.id+' has been deleted')
+  )
+)
+
+const updatePlaylistNameById = (req,res)=>(
+	Playlist.findOne({
+		where: {id: req.params.id}
+	})
+	.then(playlistInfo=>
+		playlistInfo.update({
+			title: req.body.title
+		})
+	)
+	.then(()=>
+		res.send('Playlist with Id:'+req.params.id+' updated with name: '+req.body.title+' !')
+	)
+)
 
 //ROUTES//
+router.route('/')
+  .get(getAllPlaylists)
+  .post(postNewPlaylist) // needs title
 
+router.route('/:id/:songName')
+  .put(updatePlaylistSong)
+  .delete(removePlaylistSong)
+
+router.route('/:id')
+  .get(getPlaylistById)
+  .delete(deletePlaylistById)
+  .put(updatePlaylistNameById) // needs title
 
 module.exports = router
