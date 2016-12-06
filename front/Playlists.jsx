7e9ae2bd0ec4fd7af9d1b05bab79c381a196c2ca
Jung Chen <jungwchen@gmail.com>
2016-12-06 15:38:37 -0500

    display Playlists

--- front/Playlists.jsx
+++ front/Playlists.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import {Link} from 'react-router'
+import $ from 'jquery'
 
 const Playlists = React.createClass({
+  getInitialState(){
+    return {playlists:[]}
+  },
+  componentDidMount(){
+  $.ajax({
+  	url: '/api/playlists',
+  	method: 'GET',
+  })
+  .done(data=>this.setState({playlists: data}))
+  },
+  deletePlaylist(id){
+  // $.ajax({
+  // 	url: '/api/playlists'+id,
+  // 	method: 'DELETE',
+  // })
+  console.log(id)
+  },
 	render(){
 		return(
 			<div>
-				Playlists
+			  <h2>PLAYLISTS:</h2>
+			  <h3>New Playlist</h3><Link to='/playlists/newPlaylist'><input type="button" value="+" /></Link>
+				{this.state.playlists.map((element, index)=>(
+					<div key={index}>
+						<Link to={'/playlists/'+element.id}><h3>{element.title}</h3></Link>
+						<input type="button" value="+" />
+						<input type="button" value="-" onClick={this.deletePlaylist.bind(this,element.id)}/>
+					</div>
+				))
+				}
 			</div>
 			)
 	}
