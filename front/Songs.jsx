5bfb757573460408206843f9ac24f6a8cd0f3837
electrone901 <Carbajalluis@rocketmail.com>
2016-12-06 18:51:47 -0500

    modify front song file

--- front/Songs.jsx
+++ front/Songs.jsx
@@ -43,25 +43,4 @@ export default Songs;
 
 
 
-///
-
-
-// {
-// 					return <div key={i}>
-// 								<h3>Title: {ele.title}</h3>
-// 								<h3>Artist Name: {ele.artist.name}</h3>
-// 								<h3>Artist Name: {ele.artist.name}</h3>
-// 								<div>
-// 									<iframe id="ytplayer" 
-// 										type="text/html" width="640" 
-// 										height="360"
-// 										src={`${ele.youtube_url.replace('watch?v=', 'embed/')}`}>
-// 									</iframe>
-// 								</div>
-// 					       </div>
-// 				}
-
-
-
-
 
